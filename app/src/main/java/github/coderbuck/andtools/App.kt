38a6a17367d00e425ed71cec24e2fc85533273d6
buck <coderbuck@163.com>
2020-05-12 09:23:16 +0800

    gradle

--- app/src/main/java/github/coderbuck/andtools/App.kt
+++ app/src/main/java/github/coderbuck/andtools/App.kt
@@ -1,7 +1,9 @@
 package github.coderbuck.andtools
 
 import android.app.Application
+import com.blankj.utilcode.util.CrashUtils
 import com.blankj.utilcode.util.Utils
+import java.io.File
 
 /**
  * Created by gwf on 2019/5/27
@@ -12,5 +14,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         Utils.init(this)
+        CrashUtils.init(filesDir.path + "/crash/")
     }
 }
